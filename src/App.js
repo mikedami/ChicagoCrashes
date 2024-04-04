44ac31bf6511387b8ee4eb1d8e27b8915c530a87
mikedami <damianomike12@gmail.com>
2024-04-04 14:06:35 -0400

    Created Query Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout/index.js';
 import Home from './Components/Home/index.js';
+import Query from './Components/Query/index.js';
 
 function App() {
   return(
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element = {<Home />} />
+        <Route path="/query" element={<Query />} /> 
       </Route>
     </Routes>
     </>
---
+++ src/Components/Query/index.js
@@ -0,0 +1,10 @@
+import './index.scss';
+import '../../mainstyles.scss';
+
+const Query = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default Query
