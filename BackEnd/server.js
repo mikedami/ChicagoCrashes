15e2c1775cb2ba3f027767641e99a0e0d7eb14f8
mikedami <damianomike12@gmail.com>
2024-04-17 14:45:53 -0400

    Finished Product

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -336,6 +336,61 @@ app.get('/data', (req, res) => {
 
             const GeomType = await connection.getDbObjectClass("MDSYS.SDO_GEOMETRY");
             
+            var coords = [
+                -88.096619,41.533254,
+                -87.239685,41.533254,
+                -87.239685,42.073762,
+                -88.096619,42.073762,
+                -88.096619,41.533254 
+            ];
+
+            const geom = new GeomType(
+                {
+                    SDO_GTYPE: 2003,
+                    SDO_SRID: null,
+                    SDO_POINT: null,
+                    SDO_ELEM_INFO: [ 1, 1003, 1 ],
+                    SDO_ORDINATES: coords
+                }
+            );
+
+            console.log("Created SDO_GEOMETRY object:", geom);
+
+            const query = `SELECT /*+ PARALLEL(a, 8) */ *
+            FROM TABLE(sdo_PointInPolygon(
+              CURSOR(select longitude x, latitude y, crashes.* from DCIUCULIN.crashes),
+              :shape,
+              0.0005)) a where rownum < 700`
+
+            const result = await connection.execute(query, {shape : geom}); 
+            await connection.close();
+            return result;
+
+        } catch (error) {
+            return error;
+        }
+    }
+    fetchData()
+    .then(dbRes => {
+        res.send(dbRes);
+    })
+    .catch(err => {
+        res.send(err);
+    })
+})
+
+app.post('/data', (req, res) => {
+    async function fetchData(){
+        console.log("queried!");
+        try {
+            const connection = await oracledb.getConnection({
+                user: process.argv[2], //enter username
+                password: process.argv[3], //enter password
+                connectString: 'oracle.cise.ufl.edu:1521/orcl'
+            });
+
+            const GeomType = await connection.getDbObjectClass("MDSYS.SDO_GEOMETRY");
+            
             var coords = req.body;
 
             const geom = new GeomType(
@@ -370,6 +425,7 @@ app.get('/data', (req, res) => {
         res.send(err);
     })
 })
+
 
 app.listen(PORT, 
     () => {
