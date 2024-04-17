1c4329145be5416c9865ffcba3fb8986c364d520
mikedami <damianomike12@gmail.com>
2024-04-17 10:13:06 -0400

    Query 1 and 3 done

--- src/Components/Query3/index.js
+++ src/Components/Query3/index.js
@@ -1,9 +1,55 @@
-import './index.scss'
+import './index.scss';
+import '../../mainstyles.scss';
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import axios from 'axios';
 
 const Query3 = () => {
-    return(
-        <div>Hello</div>
-    )
-}
+    const [data, setData] = useState([]);
 
-export default Query3
+    useEffect(() => {
+        axios('http://localhost:5000/impair')
+            .then(response => {
+                if (response.status === 200) {
+                    setData(response.data);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
+
+    const chartData = {
+        options: {
+            chart: {
+                id: 'crashes-chart',
+            },
+            xaxis: {
+                type: 'category',
+                categories: data.map(item => item[0]), // Years
+            },
+        },
+        series: [{
+            name: 'Crashes',
+            data: data.map(item => item[1]), // Crash counts
+        }],
+    };
+
+    return (
+        <div /*className='main-content'*/>
+                    <div className="page-container">
+            <h2>Crashes Due to Impaired Drivers Over the Years</h2>
+            <Chart
+                options={chartData.options}
+                series={chartData.series}
+                type="line"
+                height={400}
+                width={800}
+            />
+        </div>
+        </div>
+
+    );
+};
+
+export default Query3;
